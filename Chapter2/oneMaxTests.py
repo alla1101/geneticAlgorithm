29db25b8859d988f8ec41823ad06d78e99dcd70d
Alla Barakat <a2la1101@gmail.com>
2019-11-13 21:03:04 +0200

    completed chapter 2

--- Chapter2/oneMaxTests.py
+++ Chapter2/oneMaxTests.py
@@ -5,10 +5,40 @@ import datetime
 import Library.genetic as genetic
 import unittest
 
+def get_fitness(genes):
+    return genes.count(1)
+
+def display(candidate, startTime):
+    timeDiff=datetime.datetime.now()-startTime
+    # First 15 and last 15
+    print(
+        "{0}...{1}\t{2:3.2f}\t{3}".format(
+            ''.join(map(str,candidate.Genes[:15])),
+            ''.join(map(str,candidate.Genes[-15:])),
+            candidate.Fitness,
+            str(timeDiff)
+        )
+    )
+
 class OneMaxTests(unittest.TestCase):
+    
     def test(self,length=100):
         geneset=[0,1]
+        startTime=datetime.datetime.now()
 
-    def get_fitness(genes):
-        return genes.count(1)
+        def fnDisplay(candidate):
+            display(candidate,startTime)
         
+        def fnGetFitness(genes):
+            return get_fitness(genes)
+
+        optimalFitness=length
+        best=genetic.get_best(fnGetFitness,length,optimalFitness,geneset,fnDisplay)
+
+        self.assertEqual(best.Fitness,optimalFitness)
+
+    def test_benchmark(self):
+        genetic.Benchmark.run(lambda: self.test(4000))
+
+if __name__=='__main__':
+    unittest.main()
